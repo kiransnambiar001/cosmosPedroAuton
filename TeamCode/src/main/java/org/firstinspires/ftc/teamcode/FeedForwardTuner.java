939c8f8e2b902bf639583cf21c46f122ff59c006
kiransnambiar001 <kiransnambiar001@gmail.com>
2025-12-06 09:36:59 -0600

    tuners for meet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FeedForwardTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FeedForwardTuner.java
@@ -1,36 +1,36 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
-//import com.qualcomm.robotcore.hardware.Gamepad;
-//
-//@TeleOp
-//public class FeedForwardTuner extends LinearOpMode {
-//    double initVoltage = 0.0f;
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-//        double power = 0.0f;
-//        Gamepad prev = new Gamepad();
-//        DcMotorEx shooter = hardwareMap.get(DcMotorEx.class, "outtakeMotor");
-//        initVoltage = hardwareMap.voltageSensor.iterator().next().getVoltage();
-//        waitForStart();
-//        prev.copy(gamepad1);
-//        while (opModeIsActive()) {
-//            if (gamepad1.left_bumper && !prev.left_bumper) {
-//                power += 0.01f;
-//            }
-//            if (gamepad1.right_bumper && !prev.right_bumper) {
-//                power -= 0.01f;
-//            }
-//            shooter.setPower(power);
-//            prev.copy(gamepad1);
-//
-//            telemetry.addData("Power", power);
-//            telemetry.addData("Motor Voltage", initVoltage - hardwareMap.voltageSensor.iterator().next().getVoltage());
-//            telemetry.addData("Original Voltage", initVoltage);
-//            telemetry.addData("Motor Velocity", shooter.getVelocity());
-//            telemetry.update();
-//        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@TeleOp
+public class FeedForwardTuner extends LinearOpMode {
+    double initVoltage = 0.0f;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        double power = 0.0f;
+        Gamepad prev = new Gamepad();
+        DcMotorEx shooter = hardwareMap.get(DcMotorEx.class, "outtakeMotor");
+        initVoltage = hardwareMap.voltageSensor.iterator().next().getVoltage();
+        waitForStart();
+        prev.copy(gamepad1);
+        while (opModeIsActive()) {
+            if (gamepad1.left_bumper && !prev.left_bumper) {
+                power += 0.01f;
+            }
+            if (gamepad1.right_bumper && !prev.right_bumper) {
+                power -= 0.01f;
+            }
+            shooter.setPower(power);
+            prev.copy(gamepad1);
+
+            telemetry.addData("Power", power);
+            telemetry.addData("Motor Voltage", initVoltage - hardwareMap.voltageSensor.iterator().next().getVoltage());
+            telemetry.addData("Original Voltage", initVoltage);
+            telemetry.addData("Motor Velocity", shooter.getVelocity());
+            telemetry.update();
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTuner.java
@@ -1,69 +1,70 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
-//
-//@TeleOp(name="PID Tuner", group="LinearOpMode")
-//public class PIDTuner extends LinearOpMode {
-//    Hardware robotHardware = new Hardware();
-//    Outtake robotOuttake;
-//
-//    private double pVal = 0;
-//    private double iVal = 0;
-//    private double dVal = 0;
-//    private double fVal = 0.013;
-//
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-//        robotHardware.initialize(hardwareMap);
-//        robotOuttake = new Outtake(robotHardware);
-//
-//        waitForStart();
-//
-//        boolean rb2prevState = false;
-//        boolean lb2prevState = false;
-//        boolean dpu2prevState = false;
-//        boolean dpd2prevState = false;
-//
-//        while (opModeIsActive()) {
-//            boolean rb2 = gamepad2.right_bumper;
-//            boolean lb2 = gamepad2.left_bumper;
-//            boolean dpu2 = gamepad2.dpad_up;
-//            boolean dpd2 = gamepad2.dpad_down;
-//            double rt2 = gamepad2.right_trigger;
-//
-//            if (rb2 && !rb2prevState) {
-//                pVal += 0.001;
-//            } rb2prevState = rb2;
-//
-//            if (lb2 && !lb2prevState) {
-//                pVal -= 0.001;
-//            } lb2prevState = lb2;
-//
-//            if (dpu2 && !dpu2prevState) {
-//                dVal += 0.001;
-//            } dpu2prevState = dpu2;
-//
-//            if (dpd2 && !dpd2prevState) {
-//                dVal -= 0.001;
-//            } dpd2prevState = dpd2;
-//            robotHardware.outtakeMotor.setVelocityPIDFCoefficients(pVal,iVal,dVal,fVal);
-//
-//            if (rt2 >0.5) {
-//                robotOuttake.run(0.7);
-//            } else {
-//                robotOuttake.run(0);
-//            }
-//
-//            telemetry.addData("P Value", pVal);
-//            telemetry.addData("I Value", iVal);
-//            telemetry.addData("D Value", dVal);
-//            telemetry.addData("F Value", fVal);
-//            telemetry.addData("Target Velocity (tps)", robotOuttake.getTargetTps());
-//            telemetry.addData("Current Velocity (tps)", robotHardware.outtakeMotor.getVelocity());
-//            telemetry.update();
-//        }
-//
-//    }
-//}
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(name="PID Tuner", group="LinearOpMode")
+public class PIDTuner extends LinearOpMode {
+    Hardware robotHardware = new Hardware();
+    Outtake robotOuttake;
+
+    private double pVal = 135.2;
+    private double iVal = 0;
+    private double dVal = 0;
+    private double fVal = 0.013;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robotHardware.initialize(hardwareMap);
+        robotOuttake = new Outtake(robotHardware);
+
+        waitForStart();
+
+        boolean rb2prevState = false;
+        boolean lb2prevState = false;
+        boolean dpu2prevState = false;
+        boolean dpd2prevState = false;
+
+        while (opModeIsActive()) {
+            boolean rb2 = gamepad2.right_bumper; // pval+
+            boolean lb2 = gamepad2.left_bumper; // pval-
+            boolean dpu2 = gamepad2.dpad_up; // dval+
+            boolean dpd2 = gamepad2.dpad_down; // dval-
+            double rt2 = gamepad2.right_trigger; // turn on outtake to 0.7
+
+
+            if (rb2 && !rb2prevState) {
+                pVal += 1;
+            } rb2prevState = rb2;
+
+            if (lb2 && !lb2prevState) {
+                pVal -= 1;
+            } lb2prevState = lb2;
+
+            if (dpu2 && !dpu2prevState) {
+                dVal += 1;
+            } dpu2prevState = dpu2;
+
+            if (dpd2 && !dpd2prevState) {
+                dVal -= 1;
+            } dpd2prevState = dpd2;
+            robotHardware.outtakeMotor.setVelocityPIDFCoefficients(pVal,iVal,dVal,fVal);
+
+            if (rt2 >0.5) {
+                robotOuttake.run(0.7);
+            } else {
+                robotOuttake.run(0);
+            }
+
+            telemetry.addData("P Value", pVal);
+            telemetry.addData("I Value", iVal);
+            telemetry.addData("D Value", dVal);
+            telemetry.addData("F Value", fVal);
+            telemetry.addData("Target Velocity (tps)", robotOuttake.getTargetTps());
+            telemetry.addData("Current Velocity (tps)", robotHardware.outtakeMotor.getVelocity());
+            telemetry.update();
+        }
+
+    }
+}
