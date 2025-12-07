d1d6ba790a0f60b84090f1e437d27694147212a9
kiransnambiar001 <kiransnambiar001@gmail.com>
2025-12-07 12:29:53 -0600

    Updated PID Tuner to start with default coefficient values, and then be controlled or altered by panels or gamepad. Also made it so that the increment value can be controlled via panels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuners/PIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuners/PIDTuner.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.tuners;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -8,73 +9,97 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.teamcode.subsystems.Hardware;
 import org.firstinspires.ftc.teamcode.subsystems.Outtake;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
 
+@Configurable
 @TeleOp(name="PID Tuner", group="LinearOpMode")
-public class PIDTuner extends LinearOpMode {
+public class PIDTuner extends OpMode {
     Hardware robotHardware = new Hardware();
     Outtake robotOuttake;
 
-    private double pVal, fVal, dVal, iVal;
+    public static double pVal, fVal, dVal, iVal;
+    public static double increment = 0.01;
+    public TelemetryManager panels;
 
+    public PIDFCoefficients defaultCoefficients;
 
-
+    private void log(String caption, Object... text) {
+        if (text.length == 1) {
+            telemetry.addData(caption, text[0]);
+            panels.debug(caption + ": " + text[0]);
+        } else if (text.length >= 2) {
+            StringBuilder message = new StringBuilder();
+            for (int i = 0; i < text.length; i++) {
+                message.append(text[i]);
+                if (i < text.length - 1) message.append(" ");
+            }
+            telemetry.addData(caption, message.toString());
+            panels.debug(caption + ": " + message);
+        }
+    }
 
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void init() {
         robotHardware.initialize(hardwareMap);
         robotOuttake = new Outtake(robotHardware);
 
-        PIDFCoefficients coefficients = robotHardware.outtakeMotor.getPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        fVal = coefficients.f;
-        dVal = coefficients.d;
-        iVal = coefficients.i;
-        pVal = coefficients.p;
+        defaultCoefficients = robotHardware.outtakeMotor.getPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        fVal = defaultCoefficients.f;
+        dVal = defaultCoefficients.d;
+        iVal = defaultCoefficients.i;
+        pVal = defaultCoefficients.p;
 
-        waitForStart();
+        panels = PanelsTelemetry.INSTANCE.getTelemetry();
+    }
 
-        boolean rb2prevState = false;
-        boolean lb2prevState = false;
-        boolean dpu2prevState = false;
-        boolean dpd2prevState = false;
-        boolean dpr2prevState = false;
-        boolean dpl2prevState = false;
+    @Override
+    public void start() {
+    }
 
-
-        while (opModeIsActive()) {
-            boolean rb2 = gamepad2.right_bumper; // pval+
-            boolean lb2 = gamepad2.left_bumper; // pval-
-            boolean dpu2 = gamepad2.dpad_up; // dval+
-            boolean dpd2 = gamepad2.dpad_down; // dval-
-            boolean dpr2 = gamepad2.dpad_right; // ival+
-            boolean dpl2 = gamepad2.dpad_left; // ival-
-            double rt2 = gamepad2.right_trigger; // turn on outtake to 0.7
-            double lt2 = gamepad2.left_trigger;
-
-
-            if (rb2 && !rb2prevState) {pVal += 0.01;} rb2prevState = rb2;
-            if (lb2 && !lb2prevState) {pVal -= 0.01;} lb2prevState = lb2;
-
-            if (dpu2 && !dpu2prevState) {dVal += 0.01;} dpu2prevState = dpu2;
-            if (dpd2 && !dpd2prevState) {dVal -= 0.01;} dpd2prevState = dpd2;
-
-            if (dpr2 && !dpr2prevState) {iVal += 0.01;} dpr2prevState = dpr2;
-            if (dpl2 && !dpl2prevState) {iVal -= 0.01;} dpl2prevState = dpl2;
+    @Override
+    public void loop() {
+        boolean rb2wP = gamepad2.rightBumperWasPressed(); // pval+
+        boolean lb2wP = gamepad2.leftBumperWasPressed(); // pval-
+        boolean dpu2wP = gamepad2.dpadUpWasPressed(); // dval+
+        boolean dpd2wP = gamepad2.dpadDownWasPressed(); // dval-
+        boolean dpr2wP = gamepad2.dpadRightWasPressed(); // ival+
+        boolean dpl2wP = gamepad2.dpadLeftWasPressed(); // ival-
+        boolean x2wP = gamepad2.xWasPressed();
+        boolean b2wP = gamepad2.bWasPressed();
+        double rt2 = gamepad2.right_trigger;
+        double lt2 = gamepad2.left_trigger;
 
 
-//            robotHardware.outtakeMotor.setVelocityPIDFCoefficients(pVal,iVal,dVal,fVal);
+        if (rb2wP) {pVal += increment;}
+        if (lb2wP) {pVal -= increment;}
 
-            if (rt2 >0.5) {robotOuttake.run(0.1);}
-            else if (lt2 > 0.5) {robotOuttake.run(0.05);}
-            else {robotOuttake.run(0);}
+        if (dpu2wP) {dVal += increment;}
+        if (dpd2wP) {dVal -= increment;}
 
-            telemetry.addData("P Value", pVal);
-            telemetry.addData("I Value", iVal);
-            telemetry.addData("D Value", dVal);
-            telemetry.addData("F Value", fVal);
-            telemetry.addData("Target Velocity (tps)", robotOuttake.getTargetTps());
-            telemetry.addData("Current Velocity (tps)", robotHardware.outtakeMotor.getVelocity());
-            telemetry.update();
-        }
+        if (dpr2wP) {iVal += increment;}
+        if (dpl2wP) {iVal -= increment;}
 
+        if (x2wP) {fVal += increment;}
+        if (b2wP) {fVal -= increment;}
+
+        robotHardware.outtakeMotor.setVelocityPIDFCoefficients(pVal,iVal,dVal,fVal);
+
+        if (rt2 >0.5) {robotOuttake.run(0.1);}
+        else if (lt2 > 0.5) {robotOuttake.run(0.05);}
+        else {robotOuttake.run(0);}
+
+
+        // telemetry
+        log("Increment Value", increment);
+
+        log("P Value", pVal); log("I Value", iVal); log("D Value", dVal); log("F Value", fVal);
+
+        log("Target Velocity (tps)", robotOuttake.getTargetTps());
+        log("Current Velocity (tps)", robotHardware.outtakeMotor.getVelocity());
+
+        log("Default P Value", defaultCoefficients.p); log("Default I Value", defaultCoefficients.i);
+        log("Default D Value", defaultCoefficients.d); log("Default F Value", defaultCoefficients.f);
+        panels.update(telemetry);
     }
 }
